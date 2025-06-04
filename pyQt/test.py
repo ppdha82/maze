1dfd54c44f46f2e4ddebfa05919f97916877dd32
Deukhyun Park <ppdha82@gmail.com>
2025-06-04 19:36:49 +0900

    [pyQt] 예제 코드 추가
    - pyQt5 chap5 QMainWindow에 상태바 만들기 (test.py)

--- pyQt/test.py
+++ pyQt/test.py
@@ -1,7 +1,27 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QStatusBar, QMainWindow, QLabel
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
+
+option = 1
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+        self.initUI()
+
+    def initUI(self):
+        if option == 1:
+            self.statusBar.showMessage('Ready')
+        else:
+            self.label = QLabel('StatusBar Button')
+            self.statusBar.addWidget(self.label)
+
+        self.setGeometry(300, 300, 400, 300)
+        self.setWindowTitle('Status Window')
+
 
 class MyApp(QWidget):
     def __init__(self):
@@ -9,6 +29,10 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.statusbar = QStatusBar(self)
+        # self.setStatusBar(self.statusbar)
+        self.statusbar.showMessage('Ready')
+
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('this is a <b>QWidget</b> widget')
 
@@ -25,5 +49,7 @@ class MyApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyApp()
+    mainWindow = MainWindow()
+    mainWindow.show()
+    # ex = MyApp()
     sys.exit(app.exec_())
