221e2570fe7930a3f25d82fd6624f550ab97741f
Deukhyun Park <ppdha82@gmail.com>
2025-06-04 15:17:55 +0900

    [pyQt] 예제 코드 추가
    - pyQt5 윈도우 타이틀에 아이콘 추가 (pyQt/test.py)

--- pyQt/test.py
+++ pyQt/test.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
 class MyApp(QWidget):
     def __init__(self):
@@ -7,9 +8,9 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+        self.setGeometry(300, 300, 300, 200)
         self.show()
 
 if __name__ == '__main__':
