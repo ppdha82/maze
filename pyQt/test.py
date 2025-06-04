f5bb822bfcf363f6d3a8a41f939150a6e811e726
Deukhyun Park <ppdha82@gmail.com>
2025-06-04 15:33:18 +0900

    [pyQt] 예제 코드 추가
    - pyQt5 chap3 창 닫기 버튼 추가 (pyQt/test.py)

--- pyQt/test.py
+++ pyQt/test.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
 class MyApp(QWidget):
     def __init__(self):
@@ -8,9 +9,14 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle('Icon')
+        btn = QPushButton('Quit', self)
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
+
+        self.setWindowTitle('Quit Button')
         self.setWindowIcon(QIcon('web.png'))
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(300, 300, 500, 400)
         self.show()
 
 if __name__ == '__main__':
