220ff8b5b513a177b13ec78cf208b4ac87ac3600
Deukhyun Park <ppdha82@gmail.com>
2025-06-04 15:54:17 +0900

    [pyQt] 예제 코드 추가
    - pyQt5 chap4 툴팁 나타내기 추가 (pyQt/test.py)

--- pyQt/test.py
+++ pyQt/test.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
+from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
 
 class MyApp(QWidget):
@@ -9,12 +9,16 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('this is a <b>QWidget</b> widget')
+
         btn = QPushButton('Quit', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
         btn.move(50, 50)
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
 
-        self.setWindowTitle('Quit Button')
+        self.setWindowTitle('Tooltips')
         self.setWindowIcon(QIcon('web.png'))
         self.setGeometry(300, 300, 500, 400)
         self.show()
