00fe62b253a67cbf826f5a97645e76b99bcc512f
Deukhyun Park <ppdha82@gmail.com>
2025-06-09 12:02:45 +0900

    [pyQt] 예제 코드 추가
    - pyQt5 chap7 툴바 만들기 (pyQt/test.py)
    - 예제에서 사용된 이미지 파일 추가 (edit.png, exit.png, print.png,
      save.png)

--- pyQt/test.py
+++ pyQt/test.py
@@ -1,26 +1,58 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QStatusBar, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QStatusBar, QMainWindow, QLabel, QAction, qApp
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, Qt
 
-option = 1
+statusBarOption = 1 # != 1
+window_option = 'QMainwindow' # 'QWidget'
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # chap 5
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
 
         self.initUI()
 
     def initUI(self):
-        if option == 1:
+        # chap 5
+        if statusBarOption == 1:
             self.statusBar.showMessage('Ready')
         else:
             self.label = QLabel('StatusBar Button')
             self.statusBar.addWidget(self.label)
 
+        # chap 6
+        exitAction = QAction(QIcon('exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
+
+        # chap 7
+        self.toolbar = self.addToolBar('Toolbar')
+
+        saveAction = QAction(QIcon('save.png'), 'Save', self)
+        saveAction.setStatusTip('Saving')
+        self.toolbar.addAction(saveAction)
+
+        editAction = QAction(QIcon('edit.png'), 'Edit', self)
+        editAction.setStatusTip('Editing')
+        self.toolbar.addAction(editAction)
+
+        printAction = QAction(QIcon('print.png'), 'Print', self)
+        printAction.setStatusTip('Printing')
+        self.toolbar.addAction(printAction)
+
+        self.toolbar.addAction(exitAction)
+
         self.setGeometry(300, 300, 400, 300)
-        self.setWindowTitle('Menubar')
+        self.setWindowTitle('Toolbar')
 
 
 class MyApp(QWidget):
@@ -29,19 +61,18 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.statusbar = QStatusBar(self)
-        # self.setStatusBar(self.statusbar)
-        self.statusbar.showMessage('Ready')
-
+        # chap 4
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('this is a <b>QWidget</b> widget')
 
+        # chap 3
         btn = QPushButton('Quit', self)
         btn.setToolTip('This is a <b>QPushButton</b> widget')
         btn.move(50, 50)
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
 
+        # chap 2
         self.setWindowTitle('Tooltips')
         self.setWindowIcon(QIcon('web.png'))
         self.setGeometry(300, 300, 500, 400)
@@ -49,7 +80,10 @@ class MyApp(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = MainWindow()
-    mainWindow.show()
-    # ex = MyApp()
+    if window_option == 'QMainwindow':
+        mainWindow = MainWindow()
+        mainWindow.show()
+    else:
+        ex = MyApp()
+
     sys.exit(app.exec_())
