9a87e81b514ae9e072f2ca7483a91a700857d7a6
Deukhyun Park <ppdha82@gmail.com>
2025-06-23 18:15:03 +0900

    [pyQt] Widget 예제 코드 추가
    - pyQt5 chapter 05.02 QLabel 추가 (pyQt/widget.py)

--- pyQt/widget.py
+++ pyQt/widget.py
@@ -1,6 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtCore import Qt
 
+layoutOption = 'QLabel' # 'QPushButton'
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -8,23 +10,50 @@ class MyApp(QWidget):
 
     def initUI(self):
         # chapter 05.01
-        btn1 = QPushButton('&Button1', self)
-        btn1.setCheckable(True)
-        btn1.toggle()
+        if(layoutOption == 'QPushButton'):
+            btn1 = QPushButton('&Button1', self)
+            btn1.setCheckable(True)
+            btn1.toggle()
 
-        btn2 = QPushButton(self)
-        btn2.setText('Button&2')
+            btn2 = QPushButton(self)
+            btn2.setText('Button&2')
 
-        btn3 = QPushButton(self)
-        btn3.setEnabled(False)
+            btn3 = QPushButton(self)
+            btn3.setEnabled(False)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(btn1)
-        vbox.addWidget(btn2)
-        vbox.addWidget(btn3)
+            vbox = QVBoxLayout()
+            vbox.addWidget(btn1)
+            vbox.addWidget(btn2)
+            vbox.addWidget(btn3)
 
-        self.setLayout(vbox)
-        self.setWindowTitle('QPushButton')
+            self.setLayout(vbox)
+            self.setWindowTitle('QPushButton')
+
+        # chapter 05.02
+        elif(layoutOption == 'QLabel'):
+            label1 = QLabel('First Label', self)
+            label1.setAlignment(Qt.AlignCenter)
+
+            label2 = QLabel('Second Label', self)
+            label2.setAlignment(Qt.AlignVCenter)
+
+            font1 = label1.font()
+            font1.setPointSize(20)
+
+            font2 = label2.font()
+            font2.setFamily('Times New Roman')
+            font2.setBold(True)
+
+            label1.setFont(font1)
+            label2.setFont(font2)
+
+            layout = QVBoxLayout()
+            layout.addWidget(label1)
+            layout.addWidget(label2)
+
+            self.setLayout(layout)
+            self.setWindowTitle('QLabel')
+            
         self.setGeometry(300, 300, 500, 200)
         self.show()
 
