3533ee6b0ce093d60d404f87ad9ca3f4432e8242
Deukhyun Park <ppdha82@gmail.com>
2025-06-30 19:29:28 +0900

    [pyQt] Widget 예제 코드 추가
    - pyQt5 chapter 05.10 QGroupBox 추가 (pyQt/widget.py)

--- pyQt/widget.py
+++ pyQt/widget.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QCheckBox, QRadioButton, QComboBox, QLineEdit, QProgressBar, QDial, QSlider, QSplitter, QFrame, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QCheckBox, QRadioButton, QComboBox, QLineEdit, QProgressBar, QDial, QSlider, QSplitter, QFrame, QHBoxLayout, QGroupBox, QGridLayout, QMenu
 from PyQt5.QtCore import Qt, QBasicTimer
 
-layoutOption = 'QSplitter' # 'QPushButton', 'QLabel'. 'QCheckBox', 'QRadioButton', 'QComboBox', 'QLineEdit', 'QProgressBar', 'QSlider_QDial'
+layoutOption = 'QGroupBox' # 'QPushButton', 'QLabel'. 'QCheckBox', 'QRadioButton', 'QComboBox', 'QLineEdit', 'QProgressBar', 'QSlider_QDial', 'QSplitter'
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -151,6 +151,16 @@ class MyApp(QWidget):
 
             hbox.addWidget(splitter2)
             self.setLayout(hbox)
+
+        # chapter 05.10
+        elif(layoutOption == 'QGroupBox'):
+            grid = QGridLayout()
+            grid.addWidget(self.createFirstExclusiveGroup(), 0, 0)
+            grid.addWidget(self.createSecondExclusiveGroup(), 1, 0)
+            grid.addWidget(self.createNonExclusiveGroup(), 0, 1)
+            grid.addWidget(self.createPushButtonGroup(), 1, 1)
+
+            self.setLayout(grid)
     
         self.setWindowTitle(layoutOption)
         self.setGeometry(300, 300, 500, 200)
@@ -200,6 +210,92 @@ class MyApp(QWidget):
     def button_clicked(self):
         self.slider.setValue(0)
         self.dial.setValue(0)
+
+    # chapter 05.10
+    def createFirstExclusiveGroup(self):
+        groupbox = QGroupBox('Exclusive Radio Buttons')
+
+        radio1 = QRadioButton('Radio1')
+        radio2 = QRadioButton('Radio2')
+        radio3 = QRadioButton('Radio3')
+        radio1.setChecked(True)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(radio1)
+        vbox.addWidget(radio2)
+        vbox.addWidget(radio3)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+    
+    def createSecondExclusiveGroup(self):
+        groupbox = QGroupBox('Exclusive Radio Buttons')
+        groupbox.setCheckable(True)
+        groupbox.setChecked(False)
+
+        radio1 = QRadioButton('Radio1')
+        radio2 = QRadioButton('Radio2')
+        radio3 = QRadioButton('Radio3')
+        radio1.setChecked(True)
+        checkbox = QCheckBox('Independent Checkbox')
+        checkbox.setChecked(True)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(radio1)
+        vbox.addWidget(radio2)
+        vbox.addWidget(radio3)
+        vbox.addWidget(checkbox)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+    
+    def createNonExclusiveGroup(self):
+        groupbox = QGroupBox('Non-Exclusive Checkboxes')
+        groupbox.setFlat(True)
+
+        checkbox1 = QCheckBox('Checkbox1')
+        checkbox2 = QCheckBox('Checkbox2')
+        checkbox2.setChecked(True)
+        tristatebox = QCheckBox('Tri-state Button')
+        tristatebox.setTristate(True)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(checkbox1)
+        vbox.addWidget(checkbox2)
+        vbox.addWidget(tristatebox)
+        vbox.addStretch(1)
+        groupbox.setLayout(vbox)
+
+        return groupbox
+    
+    def createPushButtonGroup(self):
+        groupbox = QGroupBox('Push Buttons')
+        groupbox.setCheckable(True)
+        groupbox.setChecked(True)
+
+        pushbutton = QPushButton('Normal Button')
+        togglebutton = QPushButton('Toggle Button')
+        togglebutton.setCheckable(True)
+        togglebutton.setChecked(True)
+        flatbutton = QPushButton('Flat Button')
+        flatbutton.setFlat(True)
+        popupbutton = QPushButton('Popup Button')
+        menu = QMenu(self)
+        menu.addAction('First Item')
+        menu.addAction('Second Item')
+        menu.addAction('Third Item')
+        menu.addAction('Fourth Item')
+        popupbutton.setMenu(menu)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(pushbutton)
+        vbox.addWidget(togglebutton)
+        vbox.addWidget(flatbutton)
+        vbox.addWidget(popupbutton)
+        groupbox.setLayout(vbox)
+
+        return groupbox
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
