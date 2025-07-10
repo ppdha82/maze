7af3a31df7a63d19a1f04d7987a96d45b04866c4
Deukhyun Park <ppdha82@gmail.com>
2025-07-10 16:07:49 +0900

    [pyQt] Dialog 예제 코드 추가
    - pyQt5 chapter 06.03 QFontDialog 추가 (pyQt/dialog.py)

--- pyQt/dialog.py
+++ pyQt/dialog.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QInputDialog, QFrame, QColorDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QInputDialog, QFrame, QColorDialog, QFontDialog, QVBoxLayout, QSizePolicy, QLabel
 from PyQt5.QtGui import QColor
 
-layoutOption = 'QColorDialog'   # 'QInputDialog'
+layoutOption = 'QFontDialog'   # 'QInputDialog', 'QColorDialog'
 
 class MyApp(QWidget):
     def __init__(self):
@@ -31,13 +31,29 @@ class MyApp(QWidget):
             self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
             self.frm.setGeometry(160, 35, 100, 100)
 
+        # chapter 06.03
+        elif(layoutOption == 'QFontDialog'):
+            btn = QPushButton('Dialog', self)
+            btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+            btn.move(20, 20)
+            btn.clicked.connect(self.showDialog)
+
+            vbox = QVBoxLayout()
+            vbox.addWidget(btn)
+
+            self.lbl = QLabel('The quick brown fox jumps over the lazy dog', self)
+            self.lbl.move(160, 20)
+            self.font = self.lbl.font()
+
+            vbox.addWidget(self.lbl)
+            self.setLayout(vbox)
             pass
 
         self.setWindowTitle(layoutOption)
         self.setGeometry(300, 300, 500, 200)
         self.show()
 
-    # chapter 06.01, 06.02
+    # chapter 06.01, 06.02, 06.03
     def showDialog(self):
         if(layoutOption == 'QInputDialog'):
             text, ok = QInputDialog.getText(self, layoutOption, 'Enter your name:')
@@ -49,6 +65,12 @@ class MyApp(QWidget):
 
             if(col.isValid()):
                 self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
+        elif(layoutOption == 'QFontDialog'):
+            font, ok = QFontDialog.getFont(self.lbl.font())
+
+            if ok:
+                self.lbl.setFont(font)
+                self.fontInfo = font
             pass
 
 if __name__ == '__main__':
