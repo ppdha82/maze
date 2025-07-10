4d04d7bff5fac5341d66040a106877b2358083bf
Deukhyun Park <ppdha82@gmail.com>
2025-07-10 15:22:34 +0900

    [pyQt] Dialog 예제 코드 추가
    - pyQt5 chapter 06.02 QInputDialog 추가 (pyQt/dialog.py)

--- pyQt/dialog.py
+++ pyQt/dialog.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QInputDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QInputDialog, QFrame, QColorDialog
+from PyQt5.QtGui import QColor
 
-layoutOption = 'QInputDialog'
+layoutOption = 'QColorDialog'   # 'QInputDialog'
 
 class MyApp(QWidget):
     def __init__(self):
@@ -18,16 +19,36 @@ class MyApp(QWidget):
             self.le = QLineEdit(self)
             self.le.move(160, 35)
 
+        # chapter 06.02
+        elif(layoutOption == 'QColorDialog'):
+            col = QColor(0, 0, 0)
+
+            self.btn = QPushButton('Dialog', self)
+            self.btn.move(30, 30)
+            self.btn.clicked.connect(self.showDialog)
+
+            self.frm = QFrame(self)
+            self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
+            self.frm.setGeometry(160, 35, 100, 100)
+
+            pass
+
         self.setWindowTitle(layoutOption)
         self.setGeometry(300, 300, 500, 200)
         self.show()
 
-    # chapter 06.01
+    # chapter 06.01, 06.02
     def showDialog(self):
-        text, ok = QInputDialog.getText(self, layoutOption, 'Enter your name:')
+        if(layoutOption == 'QInputDialog'):
+            text, ok = QInputDialog.getText(self, layoutOption, 'Enter your name:')
 
-        if ok:
-            self.le.setText(str(text))
+            if ok:
+                self.le.setText(str(text))
+        elif(layoutOption == 'QColorDialog'):
+            col = QColorDialog.getColor()
+
+            if(col.isValid()):
+                self.frm.setStyleSheet('QWidget { background-color: %s }' % col.name())
             pass
 
 if __name__ == '__main__':
