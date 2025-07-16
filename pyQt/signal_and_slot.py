c630ef85e2c0ab71a11b3534e5bea5dea596aca2
Deukhyun Park <ppdha82@gmail.com>
2025-07-16 14:13:35 +0900

    [pyQt] Signal and Slt 예제 코드 추가
    - pyQt5 07.03 이벤트 핸들러 재구성하기 예제 코드 추가 (pyQt/signal_and_slot.py)

--- pyQt/signal_and_slot.py
+++ pyQt/signal_and_slot.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLCDNumber, QDial, QVBoxLayout, QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLCDNumber, QDial, QVBoxLayout, QHBoxLayout, QPushButton, QMessageBox
+from PyQt5.QtCore import Qt
 
-layoutOption = 'Event_Handler'  # 'Signal_and_Slot'
+layoutOption = 'Rebuilding_Event_Handler'  # 'Signal_and_Slot', 'Event_Handler'
 
 class MyApp(QWidget):
     def __init__(self):
@@ -44,6 +45,10 @@ class MyApp(QWidget):
             btn1.clicked.connect(self.resizeBig)
             btn2.clicked.connect(self.resizeSmall)
 
+        # chapter 07.03
+        elif layoutOption == 'Rebuilding_Event_Handler':
+            pass
+
         self.setWindowTitle(layoutOption)
         self.setGeometry(300, 300, 500, 200)
         self.show()
@@ -54,6 +59,19 @@ class MyApp(QWidget):
 
     def resizeSmall(self):
         self.resize(200, 250)
+
+    # chapter 07.03
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_Escape:
+            reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                self.close()
+        elif e.key() == Qt.Key_F:
+            self.showFullScreen()
+        elif e.key() == Qt.Key_N:
+            self.showNormal()
+
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
