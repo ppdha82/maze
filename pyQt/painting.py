98ca1fd4eaeeb64ee1ee4063bec2b1643750462b
Deukhyun Park <ppdha82@gmail.com>
2025-07-21 12:01:45 +0900

    [pyQt] 그림 그리기 예제 코드 추가
    - pyQt5 08.03 drawRect 예제 코드 추가 (pyQt/painting.py)

--- pyQt/painting.py
+++ pyQt/painting.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QPainter, QPen, QColor
-from PyQt5.QtCore import Qt, QPointF, QPoint
+from PyQt5.QtGui import QPainter, QPen, QColor, QBrush
+from PyQt5.QtCore import Qt, QPointF, QPoint, QRect, QSize
 import numpy as np
 
-layoutOption = 'drawLine'   # 'drawPoint'
+layoutOption = 'drawRect'   # 'drawPoint', 'drawLine'
 
 class MyApp(QWidget):
     def __init__(self):
@@ -12,15 +12,7 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        # chapter 08.01
-        if layoutOption == 'drawPoint':
-            pass
-
-        # chapter 08.02
-        elif layoutOption == 'drawLine':
-            pass
-            
-        self.setGeometry(300, 300, 800, 300)
+        self.setGeometry(300, 300, 1000, 300)
         self.setWindowTitle(layoutOption)
         self.show()
 
@@ -33,6 +25,9 @@ class MyApp(QWidget):
         # chapter 08.02
         elif layoutOption == 'drawLine':
             self.draw_line(qp)
+        # chapter 08.03
+        elif layoutOption == 'drawRect':
+            self.draw_rect(qp)
         qp.end()
 
     # chapter 08.01
@@ -87,19 +82,85 @@ class MyApp(QWidget):
         self.applyOffset()
         self.drawLineAndText(qp, Qt.CustomDashLine, 'Qt.CustomDashLine')
 
-        pass
-
     def drawLineAndText(self, qp, penStyle, text):
         qp.setPen(QPen(Qt.black, 3, penStyle))
         qp.drawLine(self.linePos1, self.linePos2)
         qp.drawText(self.textPos1, text)
 
     def applyOffset(self):
-        self.linePos1 += self.offset
-        self.linePos2 += self.offset
-        self.textPos1 += self.offset
+        # chapter 08.02
+        if layoutOption == 'drawLine':
+            self.linePos1 += self.offset
+            self.linePos2 += self.offset
+            self.textPos1 += self.offset
+            pass
+
+        # chapter 08.03
+        elif layoutOption == 'drawRect':
+            self.count_x += 1
+            if self.count_x == 3:
+                self.leftTopPos = QPoint(20 + self.center_x, 10) + self.offsetY
+                self.rectInfo = QRect(self.leftTopPos, QSize(100, 60))
+                self.textPos.setX(20 + self.center_x)
+                self.textPos += self.offsetY
+                pass
+            else:
+                self.leftTopPos += self.offsetX
+                self.rectInfo.moveTopLeft(self.leftTopPos)
+                self.textPos.setX(self.textPos.x() + 160)
+
+    # chapter 08.03
+    def draw_rect(self, qp):
+        qp.setBrush(QColor(180, 100, 160))
+        qp.setPen(QPen(QColor(60, 60, 60), 3))
+        qp.drawRect(20, 20, 100, 100)
+
+        qp.setBrush(QColor(40, 150, 20))
+        qp.setPen(QPen(Qt.blue, 2))
+        qp.drawRect(180, 120, 50, 120)
+
+        qp.setBrush(Qt.yellow)
+        qp.setPen(QPen(Qt.red, 5))
+        qp.drawRect(280, 30, 80, 40)
+
+        self.draw_various_rect(qp)
+
+    def draw_various_rect(self, qp):
+        self.count_x = 0
+        qp.setBrush(Qt.black)
+        qp.setPen(QPen(Qt.black, 1))
+
+        self.offsetX = QPoint(160, 0)
+        self.offsetY = QPoint(0, 100)
+        self.center_x = int(self.width() / 2)
+        self.leftTopPos = QPoint(20 + self.center_x, 10)
+        self.rectInfo = QRect(self.leftTopPos, QSize(100, 60))
+        self.textPos = QPoint(20 + self.center_x, 90)
+
+        self.drawRectAndText(qp, Qt.SolidPattern, 'Qt.SolidPattern')
+
+        self.applyOffset()
+        self.drawRectAndText(qp, Qt.Dense1Pattern, 'Qt.Dense1Pattern')
+
+        self.applyOffset()
+        self.drawRectAndText(qp, Qt.Dense2Pattern, 'Qt.Dense2Pattern')
+
+        self.applyOffset()
+        self.drawRectAndText(qp, Qt.CrossPattern, 'Qt.CrossPattern')
+
+        self.applyOffset()
+        self.drawRectAndText(qp, Qt.BDiagPattern, 'Qt.BDialPattern')
+
+        self.applyOffset()
+        self.drawRectAndText(qp, Qt.FDiagPattern, 'Qt.FDialPattern')
         pass
-    
+
+    def drawRectAndText(self, qp, patternStyle, text):
+        qp.setBrush(QBrush(patternStyle))
+        qp.drawRect(self.rectInfo)
+        qp.drawText(self.textPos, text)
+        pass
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
